3baad1ff98c57eba612aede51cf04ed08ecac961
Jake Wood <jake@Jakes-MacBook-Air.local>
2024-05-09 23:04:50 -0500

    Fixes from Nitric!

--- services/monitor.py
+++ services/monitor.py
@@ -1,13 +1,4 @@
-from nitric.resources import api, schedule, kv
-from nitric.application import Nitric
-from nitric.context import HttpContext
-
-
-import os
-import requests
 import sentry_sdk
-
-from live365.services import run_check
 
 sentry_sdk.init(
     dsn="https://71a4a79653398bbbb12ee3fcc06c30bf@o4507009248067584.ingest.us.sentry.io/4507093378924544",
@@ -20,33 +11,18 @@ sentry_sdk.init(
     profiles_sample_rate=1.0,
 )
 
-main = api("monitor")
+import os
+import requests
+
+from live365.services import run_check
+
+from nitric.resources import api, schedule, kv
+from nitric.application import Nitric
+from nitric.context import HttpContext
 
 
-@main.get("/silence")
-async def silence_detector(ctx: HttpContext):
-    secret = ctx.req.query.get("secret")
-
-    if secret[0] != os.environ.get("SILENCE_KEY"):
-        ctx.res.status = 403
-        ctx.res.body = {"error": "Invalid secret"}
-        return
-
-    requests.post("https://events.pagerduty.com/v2/enqueue", json={
-        "payload": {
-            "summary": "Silence detected",
-            "severity": "critical",
-            "source": "PiraCZ"
-        },
-        "routing_key": os.environ.get("PAGERDUTY_ROUTING_KEY"),
-        "event_action": "trigger"
-    })
-    ctx.res.status = 200
-    ctx.res.body = "ok"
-
-
-live365_previous_jwt = kv("live365monitor").allow("get", "set")
-live365_monitor_app = schedule("live365monitor")
+live365_previous_jwt = kv("live365creds").allow("get", "set")
+live365_monitor_app = schedule("live365-monitor")
 
 
 @live365_monitor_app.every("5 minutes")
@@ -61,6 +37,45 @@ async def run_live365_monitor(ctx):
         await live365_previous_jwt.set("previous_token", {"token": token})
 
     await run_check(previous_jwt, save_token)
+
+
+main = api("silence-api")
+
+
+@main.get("/healthz")
+async def healthz(ctx: HttpContext):
+    ctx.res.body = "ok"
+
+
+@main.get("/silence")
+async def silence_detector(ctx: HttpContext):
+    secret = ctx.req.query.get("secret")
+
+    if not secret:
+        ctx.res.status = 403
+        ctx.res.body = {"error": "No secret"}
+        return
+
+    try:
+        if secret[0] != os.environ.get("SILENCE_KEY"):
+            ctx.res.status = 403
+            ctx.res.body = {"error": "Invalid secret"}
+            return
+    except Exception as exc:
+        sentry_sdk.capture_exception(exc)
+        raise
+
+    requests.post("https://events.pagerduty.com/v2/enqueue", json={
+        "payload": {
+            "summary": "Silence detected",
+            "severity": "critical",
+            "source": "PiraCZ"
+        },
+        "routing_key": os.environ.get("PAGERDUTY_ROUTING_KEY"),
+        "event_action": "trigger"
+    })
+    ctx.res.status = 200
+    ctx.res.body = "ok"
 
 
 Nitric.run()
